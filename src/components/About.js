5865a28e64768532ce19e1449a95cc7e91965973
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-11-26 10:33:31 -0500

    adjust styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,30 +2,30 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id='about' className=' bg-blue-100'>
+    <section id='about' className=' bg-white'>
       <div className='container mx-auto flex px-20 md:py-20 py-10 md:flex-row flex-col items-center'>
         <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
           <h1 className='title-font sm:text-4xl text-2xl mb-4 font-medium text-gray-900'>
-            Hi, I'm Evgeniya.
+            <span className='text-blue-700'>Hello.</span> I'm Evgeniya.
             <br className='hidden md:inline-block' />{" "}
           </h1>
-          <p className='text-lg mb-2 text-gray-700 font-medium'>
+          <p className='text-lg mb-2 text-gray-800 font-medium'>
             I am a Front-end Developer.
           </p>
-          <p className='mb-8 leading-relaxed'>
+          <p className='mb-8 leading-relaxed text-gray-500'>
             I love to build clean, simple, and functional things, and I love
             what I do.
           </p>
           <div className='flex justify-center'>
             <a
               href='#contact'
-              className='inline-block text-white bg-blue-600 border-0 py-2 px-4 focus:outline-none hover:bg-blue-500 hover:text-gray-200 rounded lg:text-lg'
+              className='inline-block bg-blue-600 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded lg:text-lg text-white'
             >
-              Work With Me
+              Get in Touch
             </a>
             <a
               href='#projects'
-              className='ml-4 inline-block text-gray-200 bg-gray-800 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 hover:text-white rounded lg:text-lg'
+              className='ml-4 inline-block text-gray-200 bg-gray-900 border-0 py-2 px-4 focus:outline-none hover:bg-gray-700 hover:text-white rounded lg:text-lg'
             >
               See My Past Work
             </a>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,15 @@ import { projects } from "../data";
 
 export default function Projects() {
   return (
-    <section id='projects' className='text-gray-400 bg-gray-50 body-font'>
-      <div className='container px-5 py-10 mx-auto text-center lg:px-40'>
-        <div className='flex flex-col w-full mb-20'>
+    <section id='projects' className='text-gray-400 bg-blue-50 body-font'>
+      <div className='container lg:px-40 px-5 py-10 mx-auto text-center'>
+        <div className='flex flex-col w-full mb-14'>
           <CodeIcon className='mx-auto inline-block w-10 mb-4 text-gray-400' />
           <h1 className='sm:text-4xl text-3xl font-medium title-font mb-4 text-gray-800'>
             Apps I've Built
           </h1>
           <p className='lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            Here are a few projects I have worked on.
           </p>
         </div>
         <div className='flex flex-wrap -m-4'>
@@ -27,12 +25,12 @@ export default function Projects() {
               <div className='flex relative'>
                 <img
                   alt='gallery'
-                  className='absolute inset-0 w-full h-full object-cover object-center'
+                  className='absolute inset-0 w-full h-full object-cover object-center rounded'
                   src={project.image}
                 />
                 {/* TODO: change styling, add technologies */}
-                <div className='px-2 py-10 relative z-10 w-full h-56 border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100'>
-                  <h2 className='tracking-widest text-xs title-font font-medium text-green-400 mb-1'>
+                <div className='px-2 py-10 relative z-10 w-full h-56 border-4 border-gray-800 rounded bg-gray-900 opacity-0 hover:opacity-100'>
+                  <h2 className='tracking-widest text-xs title-font font-medium text-blue-200 mb-1'>
                     {project.subtitle}
                   </h2>
                   <h1 className='title-font text-md font-medium text-white mb-2'>
