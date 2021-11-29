f4331b8547121bafeb117aa5a9b280d9efce8321
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-11-29 09:53:11 -0500

    change styles

--- src/components/About.js
+++ src/components/About.js
@@ -6,15 +6,16 @@ const About = () => {
       <div className='container mx-auto flex px-20 md:py-20 py-10 md:flex-row flex-col items-center'>
         <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
           <h1 className='title-font sm:text-4xl text-2xl mb-4 font-medium text-gray-900'>
-            <span className='text-blue-700'>Hello.</span> I'm Evgeniya.
+            <span className='animate-pulse text-gray-600'>Hello.</span> I'm
+            Evgeniya.
             <br className='hidden md:inline-block' />{" "}
           </h1>
           <p className='text-lg mb-2 text-gray-800 font-medium'>
             I am a Front-end Developer.
           </p>
           <p className='mb-8 leading-relaxed text-gray-500'>
-            I love to build clean, simple, and functional things, and I love
-            what I do.
+            I love to build clean, simple, and functional apps, and I love what
+            I do.
           </p>
           <div className='flex justify-center'>
             <a
